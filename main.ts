f008c96e5110cbc12535a39cf8ff93d78469774d
Carl Tornberg <carltornberg@hotmail.com>
2025-11-03 21:29:51 +0100

    Added assisting init declarations

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-import { PublicKey } from "@solana/web3.js";
+import fs from "fs";
+import { homedir } from "os";
+import { Keypair, PublicKey } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
 import { ProgramOne } from "../program-one/target/types/program_one.ts"
 
 const main = async () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const programOne = anchor.workspace.TimeVault as Program<ProgramOne>;
 
+  const alice = getKeypairFromFile(homedir() + "/.config/solana/alice.json");
+  const bob = getKeypairFromFile(homedir() + "/.config/solana/bob.json");
 };
 main().catch(console.error);
+
+function getKeypairFromFile(path: string) {
+  return Keypair.fromSecretKey(
+    Uint8Array.from(
+      JSON.parse(
+        fs.readFileSync(path, "utf8").toString()
+      )
+    )
+  );
+}
