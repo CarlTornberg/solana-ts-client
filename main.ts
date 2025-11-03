0b94f0fd3d01f9989b9e97c3c6d9460b40a3c1a9
Carl Tornberg <carltornberg@hotmail.com>
2025-11-03 21:16:22 +0100

    Init commit

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js";
+import * as anchor from "@coral-xyz/anchor";
+import {TimeVault } from "../time-vault/target/types/time_vault.ts"
+
+const main = async () => {
+
+};
+main().catch(console.error);
